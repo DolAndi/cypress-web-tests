cb499135024d8ea0034c698ff81eaacdc23c3ff6
andriano.toazza <anditoazza@gmail.com>
2023-04-24 15:02:11 -0300

    new packages and configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,29 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  env: {
+    tags: ['focus']
+  },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 15000,
+  // projectId: 'b5dxop',
+  numTestsKeptInMemory: 0,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    env: {
+      allureReuseAfterSpec: true
+    },
+    baseUrl: '',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    retries: {
+      runMode: 2,
+      openMode: 0
+    }
   },
-});
+})
